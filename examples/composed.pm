fd890f4cdfbf0a45f7711b630b26a33d913dcac3
Yijun Yu <y.yu@open.ac.uk>
2018-04-10 15:18:04 +0100

    update FSE'17 example

--- examples/composed.pm
+++ examples/composed.pm
@@ -1,41 +1,41 @@
 dtmc
-const double pseen;
-const double pignore;
-const double pjk;
-const double puc;
-const double pfc;
-const double pfl;
-const double preply;
-const double pagain;
-const double r1;
-const double r3;
-const double r4;
-const double r5;
-const double r6;
-const double r7;
+
+const double a;
+const double b;
+const double c;
+const double d;
+const double e;
+const double f;
+const double g;
+const double h;
+const double i;
+const double j;
+const double k;
 
 module die
 
-s : [0..10] init 0;
+s : [0..8] init 0;
 
-[]s=0->pseen:(s'=1)+(1-pseen-pignore-pjk):(s'=2)+pjk:(s'=3)+pignore:(s'=10);
-[]s=1->1.0:(s'=10);
-[]s=2->puc:(s'=4)+pfc:(s'=5)+(1-puc-pfc-pfl):(s'=6)+pfl:(s'=7);
-[]s=3->1.0:(s'=8);
-[]s=4->1.0:(s'=8);
-[]s=5->1.0:(s'=8);
-[]s=6->1.0:(s'=8);
-[]s=7->1.0:(s'=8);
-[]s=8->preply:(s'=9)+(1-preply):(s'=10);
-[]s=9->pagain:(s'=2)+(1-pagain):(s'=10);
+[] s=0 -> a : (s'=1);
+[] s=1 -> h : (s'=8) + b : (s'=2)  + (1-b-h) : (s'=5);
+[] s=2 -> i : (s'=8) + c : (s'=3)  + (1-c-i) : (s'=6);
+[] s=3 -> j : (s'=8) + d : (s'=4)  + (1-d-j) : (s'=7);
+[] s=4 -> k : (s'=8);
+[] s=5 -> e : (s'=6) + (1-e) : (s'=8);
+[] s=6 -> f : (s'=7) + (1-f) : (s'=8);
+[] s=7 -> l : (s'=8);
+[] s=8 -> g : (s'=8);
 
 endmodule
 
 rewards "impact"
-  s=1: r1;
-  s=3: r3;
-  s=4: r4;
-  s=5: r5;
-  s=6: r6;
-  s=7: r7;
+  s=1: i1;
+  s=2: i2;
+  s=3: i4;
+  s=4: i5;
+  s=5: i6;
+  s=6: i7;
+  s=7: i8;
+  s=8: i9;
+  
 endrewards
